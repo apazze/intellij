4a82d8a4f9a042ba7acdb6a6c28b5808462efa1f
apazze <apazze@users.noreply.github.com>
2022-04-20 16:10:16 -0300

    Primeiro Commit

---
+++ src/ClassTest.java
@@ -0,0 +1,9 @@
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println("Alisson");
+        System.out.println("Teste2");
+
+        Integer valor = new Integer(30);
+
+    }
+}
---
+++ src/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+
+    private Integer idades;
+
+    public Cliente(Integer idade) {
+        this.idades = idade;
+    }
+
+    public Integer getIdades() {
+        return idades;
+    }
+
+    public void setIdades(Integer idades) {
+        this.idades = idades;
+    }
+}
